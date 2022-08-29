d4f578d3c0c797cf1985325769738dee8c5a54b4
Murari <muraril@hummingbird-tek.com>
2022-08-29 10:58:43 +0530

    problems are added

---
+++ src/BreakCountinue.java
@@ -0,0 +1,15 @@
+public class BreakCountinue
+{
+    public static void main(String[] args)
+{
+    for (int i=0;i<8;i++)
+    {
+        if(i>6)
+        {
+            break;
+        }
+        else
+            System.out.println("value is " + i);
+    }
+}
+}
